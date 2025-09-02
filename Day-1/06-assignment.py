69096fa3068621cd7f3d966cae263959efade67e
iam-manzoor <manzoor.sr@gmail.com>
2025-09-02 19:02:02 +0530

    assignment operators examples

---
+++ Day-1/06-assignment.py
@@ -0,0 +1,10 @@
+num = 10
+
+num = num + 10 
+num += 10 # 20
+num -= 2  # 18
+num *= 2  # 36
+num /= 2  # 18.0
+num **= 2 # 324.0
+num //= 2 # 162.0 (floor division assignment operator, does the round off to the nearest intiger)
+num %= 2  # Modulus assignment opertor. Assign the remainder back to the num
