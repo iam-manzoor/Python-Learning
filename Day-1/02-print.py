b608e8dc9eff8c067d2e810e689289e3ff0ca3bf
iam-manzoor <manzoor.sr@gmail.com>
2025-09-02 11:26:59 +0530

    rename the file and update the script

---
+++ Day-1/02-print.py
@@ -0,0 +1,25 @@
+# Print function Examples
+
+# Prints the statement line-by-line. If you want the statements to be in different lines
+print("My name is Manzoor")  
+print("I am a DevOps Engg")
+
+# Prints in a single line.
+print("My name is Manzoor", "I am a DevOps Engg")
+
+# Print numbers
+print(41)
+print(31)
+
+# Perform Calculation using print function
+print(41+31)
+print(2*3)
+
+# Print the data type of a variable
+name = "Arhan"
+age = 10
+height = 4.5
+
+print(type(name))    # output  <class 'str'>
+print(type(age))     # output  <class 'int'>
+print(type(height))  # output  <class 'float'>
