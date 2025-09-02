86cb3206d7dd853ff5e5c6266fcce5e95a670da9
iam-manzoor <manzoor.sr@gmail.com>
2025-09-02 09:55:46 +0530

    print function example

---
+++ Day-1/02.print.py
@@ -0,0 +1,16 @@
+# Print function Examples
+
+# Prints the statement line-by-line. If you want the statements to be in different lines
+print("My name is Manzoor")  
+print("I am a DevOps Engg")
+
+# Prints in a single line.
+print("My name is Manzoor", "I am a DevOps Engg")
+
+# Print numbers
+print(41)
+print(31)
+
+# Perform Calculation using print function
+print(41+31)
+print(2*3)
