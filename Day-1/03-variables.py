4601c295939af72c430fe78ae3572453f1cad142
iam-manzoor <manzoor.sr@gmail.com>
2025-09-02 10:34:06 +0530

    variables with examples

---
+++ Day-1/03-variables.py
@@ -0,0 +1,21 @@
+# String should be enclosed within double quote. It can be single word or multiple words or sentence.
+name = "Manzoor"
+occupation = "DevOps" 
+
+# Intiger defined without any quote
+age = 31
+
+# Float
+price = 41.99
+
+# When you use double quote within parentnesis it prints the string as is
+print("name") # OUTPUT = name
+
+# When you want to access the value stored in a variable, avoid using double quote
+print(name, occupation) # OUTPUT = Manzoor, DevOps
+print(age)
+print(price)
+
+# Print text and the variable to the console
+print("My name is : ", name)
+print("My occupation is  : ", occupation)
