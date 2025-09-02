2086949c7472363f129d24f7717a381a9a48418d
iam-manzoor <manzoor.sr@gmail.com>
2025-09-02 18:30:49 +0530

    arithmetic examples

---
+++ Day-1/04-arithmetic.py
@@ -0,0 +1,9 @@
+a = 10
+b = 2
+
+print(a + b) # sum = 12
+print(a - b) # diff = 8
+print(a * b) # mul = 20
+print(a / b) # div = 5
+print(a % b) # modulo gives remainder 0
+print(a ** b) # a to the power of b = 100
