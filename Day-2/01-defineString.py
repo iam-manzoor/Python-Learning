92a88163b6784fe49fd3d88b880610107999ffa7
iam-manzoor <manzoor.sr@gmail.com>
2025-09-03 11:24:01 +0530

    define string examples

---
+++ Day-2/01-defineString.py
@@ -0,0 +1,14 @@
+# Will see different ways to define strings in Python
+
+string1 = "Arhan"
+string2 = 'I am in 4th std'
+string3 = """I live in Banglore"""
+string4 = '''I love playing outdoor games
+My favourite is cricket'''
+
+print(string1, string2, string3, string4)
+
+# Print Arhan's favourite sport is cricket. When you want to use single quote enclose the string in double quote and vice versa
+sport = "Arhan's favourite sport is cricket"
+
+print(sport)
