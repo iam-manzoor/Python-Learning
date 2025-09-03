6dca1783bd665f2c293d2698156d8d6971c587b8
iam-manzoor <manzoor.sr@gmail.com>
2025-09-03 09:14:42 +0530

    Adding print statement

--- Day-1/06-assignment.py
+++ Day-1/06-assignment.py
@@ -1,10 +1,17 @@
 num = 10
 
-num = num + 10 
+#num = num + 10 
 num += 10 # 20
+print(num)
 num -= 2  # 18
+print(num)
 num *= 2  # 36
+print(num)
 num /= 2  # 18.0
+print(num)
 num **= 2 # 324.0
+print(num)
 num //= 2 # 162.0 (floor division assignment operator, does the round off to the nearest intiger)
+print(num)
 num %= 2  # Modulus assignment opertor. Assign the remainder back to the num
+print(num)
