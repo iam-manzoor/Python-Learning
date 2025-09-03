3323cfec1482431917da84b6653c6aae9eb652eb
iam-manzoor <manzoor.sr@gmail.com>
2025-09-03 10:21:52 +0530

    input examples

---
+++ Day-1/10-input.py
@@ -0,0 +1,9 @@
+# Script to get input values from user
+
+name = input("Enter your name : ")          # Store the input as string
+age  = int(input("Enter your age :"))       # store the input as int
+marks = float(input("Enter your marks :"))  # store the input as float
+
+print(type(name), name)
+print(type(age), age)
+print(type(marks), marks)
