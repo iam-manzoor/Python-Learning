fbb5b3889c978267ab40eda272fc1fb669fe126d
iam-manzoor <manzoor.sr@gmail.com>
2025-09-03 10:03:22 +0530

    typeconversion examples

---
+++ Day-1/08-typeconversion.py
@@ -0,0 +1,9 @@
+"""
+Type Conversion converts one data type into another automatically
+Intiger will be converted to float automatically
+"""
+a = 10
+b = 5.25
+
+sum = a + b  # 10.0 + 5.25
+print(sum)   # 15.25
