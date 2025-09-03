8c5b71403148c00e685ebdd3b32f8f64b00dccd2
iam-manzoor <manzoor.sr@gmail.com>
2025-09-03 11:37:06 +0530

    stingOperations examples

---
+++ Day-2/02-stingOperations.py
@@ -0,0 +1,18 @@
+# Concatenate strings. Combining two or more strings are called concatenation.
+
+name = "Arhan"
+firstName = "Muhammad"
+lastName = "Manzoor"
+fullName = firstName+" "+name+" "+lastName
+print(fullName) # O/P Muhammad Arhan
+
+#====================
+# Lenght of string
+print(len(firstName)) # Print the count of total characters in the string
+
+len1 = len(name)
+print(len1)
+
+len2 = len(fullName)
+print(len2)           # Print the count of total characters in the string including spaces, special character etc..
+#====================
