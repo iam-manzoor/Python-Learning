8be2a4c978462258c95702a8f691e1827681908c
iam-manzoor <manzoor.sr@gmail.com>
2025-09-06 22:37:43 +0530

    Create 01-practice.py

---
+++ Day-3/01-practice.py
@@ -0,0 +1,17 @@
+# Program to take the user 3 favorite movie and store it in the list.
+
+# Method-1
+favoriteMovie = []
+count = 0
+
+while count < 3:
+  movie = input("Enter the movie name :")
+  favoriteMovie.append(movie)
+  count += 1
+
+print(My favorite movies are : ", favoriteMovie)
+
+# Method-2
+
+favoritMovies = input("Enter the movie names with comma seperated :").split(',') # movie1.movie2,movie3
+print(favoriteMovies)
