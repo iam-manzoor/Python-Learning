b362eea994e11ca3a2087ba5398786e6d19a0b32
iam-manzoor <manzoor.sr@gmail.com>
2025-09-06 17:53:14 +0530

    defineList examples

---
+++ Day-3/01-defineList.py
@@ -0,0 +1,20 @@
+# Will see how to define List in python
+
+# List can store different data types such as string, int, float, another list, dict.
+diff_elements["Arhan", 12, 3.9]
+print(diff_elements)
+print(type(diff_elements)
+
+# List stores int data types
+marks = [100,99,98,97,96]
+
+# Print List
+print(marks)
+
+# Lenght of List. Same as string
+print(len(marks))
+
+# Indexing in List. Simillar to string, starts from 0
+print(marks[0])  # Prints 100
+print(marks[4])  # Prints 96
+
