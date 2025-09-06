d23e6ae1c294c3c444a3e720a952eb751b306dd4
iam-manzoor <manzoor.sr@gmail.com>
2025-09-06 22:04:40 +0530

    Update 03-listMethods.py

--- Day-3/03-listMethods.py
+++ Day-3/03-listMethods.py
@@ -3,14 +3,23 @@
 marks = [89,53,98,42,67]
 
 marks.append(71)             # Adds the element at the end of the list. [89,53,98,42,67,71]
+print(marks)
 marks.sort()                 # Sort the list in ascending order 
+print(marks)
 marks.sort(reverse=True)     # Same as above sort
+print(marks)
 marks.reverse()              # Prints the list in the reverse order
+print(marks)
 marks.insert(3,75)           # Insert the element 75 in the third index of the list. [89,53,98,75,42,67,71]
+print(marks)
 marks.pop()                  # Removes the last element from the list.
+print(marks)
 marks.pop(2)                 # Removes the element which is in index 2
-marks.count(42)              # Prints the count of the given element in the list.
+print(marks)
+marks.count(42)              # Prints the count of the given element from the list.
+print(marks)
 marks.clear()                # Empty the list.
+print(marks)
 
 # Methods supported by Python list
 print(dir(list))
