2d01f9df660ea5fc4419d7d443e6e34fb490762b
iam-manzoor <manzoor.sr@gmail.com>
2025-09-06 23:07:42 +0530

    Create 03-practice.py

---
+++ Day-3/Practice/03-practice.py
@@ -0,0 +1,8 @@
+# Find the grade "A" students count form the below tuple.
+
+grade = ("C","D","A","A","B","B","A")
+
+print(grade.count("A"))
+print(grade.count("B"))
+print(grade.count("C"))
+print(grade.count("D"))
