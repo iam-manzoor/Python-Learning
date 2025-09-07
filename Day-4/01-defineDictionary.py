dbee651cb3e3ba90158fab7ad14cc4d3c787a343
iam-manzoor <manzoor.sr@gmail.com>
2025-09-07 12:48:59 +0530

    Update 01-defineDictionary.py

--- Day-4/01-defineDictionary.py
+++ Day-4/01-defineDictionary.py
@@ -1,9 +1,10 @@
 # Dict examples
 
-info = {"name" : "Arhan", "age" : 9, "height" : 3.9}
+info = {"name" : "Arhan", "age" : 9, "height" : 3.9, 12:"Intiger", 9.0 : "FLOAT"}
 
 # Print Dictionary
 print(info)
 
 # Print particular value using "Key"
-print(info["name"])
+print(info["name"])    # Prints Arhan
+print(info[12])        # Prints Intiger
