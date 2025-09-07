71541198e717bcbfe57477005c8b1fe2ae203bb8
iam-manzoor <manzoor.sr@gmail.com>
2025-09-07 12:59:56 +0530

    Update 01-defineDictionary.py

--- Day-4/01-defineDictionary.py
+++ Day-4/01-defineDictionary.py
@@ -8,3 +8,19 @@ print(info)
 # Print particular value using "Key"
 print(info["name"])    # Prints Arhan
 print(info[12])        # Prints Intiger
+
+# Assign new Value to the existing Key
+info["name"] = "Muhammad Arhan"
+print(info)
+
+# Adding new key value pair
+info["Country"] = "INDIA"
+print(info)
+
+# Create an empty Dictionary
+details= {}
+print(details)
+
+# Adding values to the empty dict
+details["name"] = "Arjun"
+details["Age"]  = 12
