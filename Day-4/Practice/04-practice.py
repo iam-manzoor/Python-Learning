c36944f77b839c02edeaa5741acf65c4bdcfd2e3
iam-manzoor <manzoor.sr@gmail.com>
2025-09-08 11:44:39 +0530

    Update 04-practice.py

--- Day-4/Practice/04-practice.py
+++ Day-4/Practice/04-practice.py
@@ -1,1 +1,13 @@
 
+# Figure out a way to store 9 & 9.0 as seperate values in set. (Take help of built-in data types)
+
+# Method 1
+num = {9,"9.0"}
+
+print(num)
+
+# Method 2
+
+num = {("int",9),("float",9.0)}
+
+print(num)
