bc82753ae9dcdcb0b3346269d260959c38a6e47b
iam-manzoor <manzoor.sr@gmail.com>
2025-09-08 11:18:26 +0530

    Create 01-practice.py

---
+++ Day-4/Practice/01-practice.py
@@ -0,0 +1,10 @@
+# Store the String
+
+meaning = {
+  "table" : ["a piece of furniture","list of facts & figures"],
+  "cat" : "a small animal"
+}
+
+print(meaning)
+print(meaning["table"])
+print(meaning["cat"])
