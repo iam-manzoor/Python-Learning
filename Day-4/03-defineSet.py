a813ddd3f400c4d9aa8b80b0b5f9a06cf4e20a05
iam-manzoor <manzoor.sr@gmail.com>
2025-09-08 10:45:12 +0530

    Create 03-defineSet.py

---
+++ Day-4/03-defineSet.py
@@ -0,0 +1,15 @@
+# Set examples
+
+collection = {1,2,3,4,5}
+
+print(collection)
+print(type(collection))
+print(len(collection))
+
+collection2 = {1,2,3,4,4,4,2,"Hello","World","Hello"}
+
+print(collection2)          # Set is unordered o/p of print varies every time it executes.
+print(len(collection2))
+
+# Empty Set
+null_set = set()            # Define empty set
