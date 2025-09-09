915ef9538d02b7cbe677a22e92a39e396ebf93ff
iam-manzoor <manzoor.sr@gmail.com>
2025-09-09 13:56:22 +0530

    Update 07-practice.py

--- Day-5/Practice/07-practice.py
+++ Day-5/Practice/07-practice.py
@@ -1,1 +1,12 @@
+# Search the element from the tuple 
 
+tup = (1,4,9,16,25,36,49,64,81,100)
+x = 49
+
+idx = 0
+for val in tup:
+  if val == x:
+    print(f"We found you {val} at the {idx} index")
+    break
+  idx += 1
+    
