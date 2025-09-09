4659ddf3dfbe66c1046a8e36beeb8a81beae83da
iam-manzoor <manzoor.sr@gmail.com>
2025-09-09 14:07:48 +0530

    Create 05-rangeFunction.py

---
+++ Day-5/05-rangeFunction.py
@@ -0,0 +1,26 @@
+# Example of range function
+
+seq = 5
+
+for i in seq:
+  print(i)
+
+for num in range(10):
+  print(num)
+  
+for num in range(1,10):
+  print(num)
+
+for num in range(1,10,2):
+  print(num)
+
+# print even numbers
+
+for num in range(2,100,2):
+  print(num)
+
+# print odd numbers
+
+for num in (1,100,2):
+  print(num)
+
