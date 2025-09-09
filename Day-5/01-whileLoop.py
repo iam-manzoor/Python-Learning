e9ea0f4b3e59b2c6427dcbf6dc0e9e903b1f2118
iam-manzoor <manzoor.sr@gmail.com>
2025-09-09 08:34:27 +0530

    Update 01-whileLoop.py

--- Day-5/01-whileLoop.py
+++ Day-5/01-whileLoop.py
@@ -1,1 +1,24 @@
 # Example while Loop
+
+count = 1          # Iterator. The var is Iterator
+
+while count <= 5:  # Iteration. Loop is iteration
+  print("Hello")
+  count += 1
+
+print(count)       # Prints 6
+
+# Print the number
+i = 1 
+
+while i <= 5:
+  print(i)
+  count += 1
+
+# Print the number in reverse  5,4,3,2,1
+
+j = 5
+
+while j > 0:
+  print(j)
+  j -= 1
