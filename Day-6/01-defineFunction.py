5408dec0e5e9a9ba62a050c38189a18aad67cf5e
iam-manzoor <manzoor.sr@gmail.com>
2025-09-10 09:52:04 +0530

    Create 01-defineFunction.py

---
+++ Day-6/01-defineFunction.py
@@ -0,0 +1,11 @@
+# Function examples
+
+def cal_sum(a,b):   # Parameters(a,b)
+  sum = a + b
+  print(sum)
+  return sum
+
+cal_sum(10,5)
+print(cal_sum)
+cal_sum(9,4)        # Arguments (9,4)
+print(cal_sum)
