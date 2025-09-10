338c2c00af60a41b2f72007eb2d19057365406e3
iam-manzoor <manzoor.sr@gmail.com>
2025-09-10 11:41:44 +0530

    Create 07-practice.py

---
+++ Day-6/Practice/07-practice.py
@@ -0,0 +1,11 @@
+# Recusive fun to print all elements in a list
+
+def print_lst(list, idx=0):
+  if idx == len(list)):
+    return
+  print(list[idx])
+  print_list(list, idx+1)
+
+fruits = ["mango","litchi", "apple", "banana"]
+
+print_list(fruits)
