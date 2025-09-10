80d934a9d24ccd01c529e050d0b80f4ba71ffa36
iam-manzoor <manzoor.sr@gmail.com>
2025-09-10 10:39:06 +0530

    Update 02-practice.py

--- Day-6/Practice/02-practice.py
+++ Day-6/Practice/02-practice.py
@@ -1,7 +1,15 @@
 # Print the elements of the list in a single line.
 
+# Method 1
+# * is the unpacking operator. Unpacks the list so the element passed as a seperate arguments.
 def lst_single_line(lst):
   print(*lst,sep=",")
 
 lst_single_line([1,2,3,4,5])
   
+# Method 2
+def lst_single_line(lst):
+  for i in lst:
+    print(i,end=" ")
+
+lst_single_line([1,2,3,4,5])
