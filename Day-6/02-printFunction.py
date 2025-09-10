288906d6ae1491a51c0b0b388e86196976db3261
iam-manzoor <manzoor.sr@gmail.com>
2025-09-10 10:06:51 +0530

    Create 02-printFunction.py

---
+++ Day-6/02-printFunction.py
@@ -0,0 +1,11 @@
+# Print function examples
+
+# Default space between words sep = " "
+print("Muhammad","Arhan")     # o/p Muhammad Arhan. By default print add space between two words when its seperated by comma
+
+# end = "\n". Both the words printed in two different lines because "\n" is the end statement
+print("Muhammad")
+print("Arhan")
+
+print("Muhammad", end="$")  # Prints Muhammad$Arhan. end value can be anything eg " ", "&" etc..
+print("Arhan")
