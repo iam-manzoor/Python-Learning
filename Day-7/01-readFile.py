1e7bfc9a442aeafffd9d16c2160a3bcd4ef01943
iam-manzoor <manzoor.sr@gmail.com>
2025-09-11 10:14:10 +0530

    Update 01-readFile.py

--- Day-7/01-readFile.py
+++ Day-7/01-readFile.py
@@ -1,8 +1,57 @@
 # Example to open a file and print the data.
 
+# Simple open file and close it
 
 file = open("demo.txt", "r")
-data = f.read()
+
+file.close()
+
+# file.read() print the whole file data
+
+file = open("demo.txt", "r")
+
+data = file.read()
 print(data)
 print(type(data))
-f.close()
+
+file.close()
+
+# file.read() speciifc character
+
+file = open("demo.txt", "r")
+
+data = file.read(5)   # Reads only the first 5 character of the file.
+print(data)
+
+data2 = file.read()   # Reads the remaining characters from the file.
+print(data2)
+
+file.close()
+
+#file.readlines() Reads the data line by line.
+
+file = open("demo.txt", "r")
+
+line1 = file.readline()
+print(line1)
+
+line2 = file.readline()
+print(line2)
+
+file.close()
+
+# Once the data read can not be read again unless we open the file again.
+
+file = open("demo.txt", "r")
+
+data = file.read()            # Reads the whole content of the file
+print(data)                   # Prints the whole content
+
+line1 = file.readline() 
+print(line1)                  # Prints the empty space, because the content is already read
+
+line2 = file.readline()
+print(line2)                  # Prints the empty space, because the content is already read
+
+file.close()
+
