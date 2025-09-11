0ffb80e077ccca922cc4f9f8c41af31bffe8d4a9
iam-manzoor <manzoor.sr@gmail.com>
2025-09-11 11:02:21 +0530

    Create 01-practice.py

---
+++ Day-7/Practice/01-practice.py
@@ -0,0 +1,7 @@
+# Creta a file and write the content
+
+with open("practice.txt", "w") as f:
+  f.write("Hi everyone\n")
+  f.write("we are learning File I/O\n")
+  f.write("using java\n")
+  f.write("I like programming in Java")
