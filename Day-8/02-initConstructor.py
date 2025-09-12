f7c013e07a750975ce25f873ff2a5c19af380a61
iam-manzoor <manzoor.sr@gmail.com>
2025-09-12 12:14:05 +0530

    Update 02-initConstructor.py

--- Day-8/02-initConstructor.py
+++ Day-8/02-initConstructor.py
@@ -1,13 +1,20 @@
 # Python Constructor within class
 
 class Student():
-  def __init__(self, fullname):                # self parameter is reference. Not necessary its should name as self. It can be abcd.
-    self.name = fullname
+
+  # Default Constructor
+  def __init__(self):
+    print("Default Constructor")                      # Either print or paas statment. 
+
+  # Parameterized Constructor
+  def __init__(self, name, marks):                    # self parameter is reference. Not necessary its should name as self. It can be abcd.
+    self.name = name                                  # self.name newly created in the object, name is from parameter section.
+    self.marks = marks
     print("Adding student to the database")
 
-s1 = Student("Arhan")
+s1 = Student("Arhan", 97)                             # Calls parameterized constructor because it has paramater
 print(s1.name)
 
-s2 = Student("Amyra")
+s2 = Student("Amyra", 88)
 print(s2.name)
     
