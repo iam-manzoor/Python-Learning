eed659eb0f32ddb403184d0ef6e232bad6985cfc
iam-manzoor <manzoor.sr@gmail.com>
2025-09-12 11:39:47 +0530

    Create 01-classobjectDefine.py

---
+++ Day-8/01-classobjectDefine.py
@@ -0,0 +1,11 @@
+# Will define class and object in this program
+
+# Class Definition
+class Student():
+  name = "Arhan"
+
+# Object Creation
+s1 = Student()       # Instance of the class.
+print(s1)            # Prints the memory location of the object.
+print(s1.name)       # Prints the value of the name var.
+
