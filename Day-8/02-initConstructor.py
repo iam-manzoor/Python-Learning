91b5d04b8ea09a0d70b1c5cf749db3d77f6264d9
iam-manzoor <manzoor.sr@gmail.com>
2025-09-12 12:27:18 +0530

    Update 02-initConstructor.py

--- Day-8/02-initConstructor.py
+++ Day-8/02-initConstructor.py
@@ -2,6 +2,9 @@
 
 class Student():
 
+  college_name = "Bharathiar University"              # class attribute
+  name = "anonymous"                                  # obj.attr takes precedence if the same name defined in the class attribute.
+
   # Default Constructor
   def __init__(self):
     print("Default Constructor")                      # Either print or paas statment. 
@@ -9,12 +12,14 @@ class Student():
   # Parameterized Constructor
   def __init__(self, name, marks):                    # self parameter is reference. Not necessary its should name as self. It can be abcd.
     self.name = name                                  # self.name newly created in the object, name is from parameter section.
-    self.marks = marks
+    self.marks = marks                                # object attribute
     print("Adding student to the database")
 
 s1 = Student("Arhan", 97)                             # Calls parameterized constructor because it has paramater
 print(s1.name)
+print(s1.college_name)
 
 s2 = Student("Amyra", 88)
 print(s2.name)
+print(Student.college_name)
     
