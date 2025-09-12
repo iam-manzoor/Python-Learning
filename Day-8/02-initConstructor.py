24994afce2c8023af9c961d142cfa9eb6ae51d03
iam-manzoor <manzoor.sr@gmail.com>
2025-09-12 11:58:09 +0530

    Create 02-initConstructor.py

---
+++ Day-8/02-initConstructor.py
@@ -0,0 +1,10 @@
+# Python Constructor within class
+
+class Student():
+  def __init__(self, fullname):                # self parameter is reference. Not necessary its should name as self. It can be abcd.
+    self.name = fullname
+    print("Adding student to the database")
+
+s1 = Student("Arhan")
+print(s1.name)
+    
