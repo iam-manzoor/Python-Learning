0d657814175caa175b74828e54f20e1ed5654798
iam-manzoor <manzoor.sr@gmail.com>
2025-09-13 21:50:07 +0530

    Create 07-superMethod.py

---
+++ Day-9/07-superMethod.py
@@ -0,0 +1,25 @@
+# super() method example.
+
+class car():                  # Parent class
+
+  def __init__(self, type):
+    self.type = type
+    print(f"{self.type} is future")
+  
+  @staticmethod
+  def start():
+    print("Car Started..")
+
+  @statcimethod
+  def stop():
+    print("Car Stopped..")
+
+class Toyata(car):              # Child class Inheritance to parent class
+
+  def __init__(self,name,type):
+    super().__init__(type)
+    self.name = name
+
+car1 = Toyata("Fortuner","electric")
+print(car1.name)
+car1.type
