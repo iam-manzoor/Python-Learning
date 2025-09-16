b1099d55726922b26dc6db137ca9b06803eb2cd3
iam-manzoor <manzoor.sr@gmail.com>
2025-09-16 11:06:07 +0530

    Create 03-practice.py

---
+++ Day-9/Practice/03-practice.py
@@ -0,0 +1,14 @@
+# DUnder function
+
+class Order:
+  def __init__(self, item, orice):
+    self.item = item
+    self.price = price
+
+  def __gt__(self, odr2):
+    return self.print > odr2.price
+
+odr1 = Order("Chips", 20)
+odr2 = Order("tea", 15)
+
+print(odr1 > odr2)
